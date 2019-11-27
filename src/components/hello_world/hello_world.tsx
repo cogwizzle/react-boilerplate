e71918ea20c439673448db060bb9e4f34714737c
Joe Fehrman <joseph.fehrman@gmail.com>
2019-11-27 04:40:40 -0500

    Adding react generator to the project.

--- src/components/hello_world/hello_world.tsx
+++ src/components/hello_world/hello_world.tsx
@@ -1,11 +1,22 @@
-import * as React from 'react';
-import './hello_world.css';
+import * as React from 'react'
+import './hello_world.css'
 
-const HelloWorld = () => (
-  <div className="hello_world">
-    Hello World<span class="hello_world__sub">!</span>
-  </div>
-);
+interface iHelloWorldProps {
 
-export default HelloWorld;
+}
+
+const HelloWorld: React.FunctionComponent<iHelloWorldProps> = () => {
+  // State
+
+  // Effects
+
+  return (
+    <div className="hello_world">
+      New HelloWorld component.
+    </div>
+  )
+}
+
+export default HelloWorld
+export { iHelloWorldProps }
 
