b291a9241fc5fd01051188b176c6eee42c881ee5
Joe Fehrman <joseph.fehrman@gmail.com>
2019-11-30 06:03:07 -0500

    Added storybook support!

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+module.exports = ({ config, mode }) => {
+  // Excluding existing CSS rules in the default storybook webpack config.
+  const isTestToBeExcluded = (test) => (test.source == '\\.css$' || test.source == '\\.module\\.css$');
+  const excludedTest = config.module.rules.filter(({ test }) => isTestToBeExcluded(test));
+  config.module.rules = config.module.rules.filter(({ test }) => !isTestToBeExcluded(test));
+
+  config.module.rules.push({
+    test: /\.css$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          plugins: [
+            require('tailwindcss'),
+            require('autoprefixer'),
+            require('postcss-nested')
+          ],
+        },
+      },
+    ],
+  });
+
+  config.module.rules.push({
+    test: /\.ts(x)?$/,
+    use: [
+      'ts-loader',
+      {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-typescript', { allExtensions:true, isTSX: true }],
+            '@babel/preset-react',
+            '@babel/preset-env',
+          ],
+        }
+      },
+    ],
+    exclude: /node_modules/,
+  });
+
+  return config;
+};
+
